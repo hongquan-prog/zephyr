56b7b354f2402aea6f13e43daf5b72108e77d89b
Hongquan Li <hongquan.li@processmission.com>
2026-07-10 13:26:52 +0800

    boards: ambiq: apollo4p_evb: add QEMU simulation support
    Add QEMU runner configuration for the apollo4p_evb board so it can be
    used with twister. The default SD image path is /../sd.img
    and can be overridden via AMBIQ_APOLLO4P_EVB_QEMU_SD_IMG.
    
    Signed-off-by: Hongquan Li <hongquan.li@processmission.com>

--- boards/ambiq/apollo4p_evb/board.cmake
+++ boards/ambiq/apollo4p_evb/board.cmake
@@ -4,3 +4,19 @@
 board_runner_args(jlink "--device=AMAP42KK-KBR" "--iface=swd" "--speed=1000")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+if(CONFIG_BOARD_APOLLO4P_EVB)
+  set(SUPPORTED_EMU_PLATFORMS qemu)
+  set(QEMU_CPU_TYPE_${ARCH} cortex-m4)
+
+  if(NOT DEFINED AMBIQ_APOLLO4P_EVB_QEMU_SD_IMG)
+    set(AMBIQ_APOLLO4P_EVB_QEMU_SD_IMG ${ZEPHYR_BASE}/../sd.img)
+  endif()
+
+  set(QEMU_FLAGS_${ARCH}
+    -machine ambiq-apollo4p-evb
+    -drive file=${AMBIQ_APOLLO4P_EVB_QEMU_SD_IMG},format=raw,if=sd,index=0
+  )
+
+  include(${ZEPHYR_BASE}/boards/common/qemu.board.cmake)
+endif()
